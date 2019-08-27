b8f216d8cb2978d86d7cf129da102a8c45e26476
Katharina Egler <katharina.egler@gmail.com>
2019-08-27 10:54:23 -0400

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :circles, only: [:index, :new, :create]
+
+  resources :updates, only: [:index, :new, :create]
+
+  resources :slots, only: [:index, :new, :create, :update]
+
+  #resources :wishlists, only: [:index, :new, :create, :update]
+
+  resources :conversations, only: [:index, :new, :create, :show] do
+  resources :messages, only: [:create]
+    end
+
 end
