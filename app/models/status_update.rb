8cccf7454d775fb9c0070456d9d833addbd7c9b9
Katharina Egler <katharina.egler@gmail.com>
2019-08-28 09:51:24 -0400

    photo uploader completed

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -2,7 +2,7 @@ class StatusUpdate < ApplicationRecord
   belongs_to :user
   has_many :circle_updates
 
-  #mount_uploader :photo, PhotoUploader
+  mount_uploader :photo, PhotoUploader
 
   validates :title, presence: true
   validates :content, presence: true
