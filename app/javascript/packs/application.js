f272dbdcfe39bbfc3359d55bc4ee6b61dec55910
Katharina Egler <katharina.egler@gmail.com>
2019-08-30 16:05:52 -0400

    heroku preparation completed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,14 @@ import "bootstrap";
 global.jQuery = jQuery
 
 const edit = document.querySelector(".add-members-form");
-edit.style.display = "none";
-const edit_btn = document.querySelector("#edit-circle-button")
+if (edit){
 
-edit_btn.addEventListener("click", (event) => {
-  event.preventDefault()
-  edit.style.display = "flex";
-});
+  edit.style.display = "none";
+  const edit_btn = document.querySelector("#edit-circle-button")
+
+  edit_btn.addEventListener("click", (event) => {
+    event.preventDefault()
+    edit.style.display = "flex";
+  });
+
+}
